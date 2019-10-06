812fd76464d713ef785791f5fab2cf59feb9427b
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-10-06 08:57:48 +0300

    authentication

---
+++ Components/Login/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+
+// NativeBase Components
+import {
+  Text,
+  Button,
+  Body,
+  List,
+  ListItem,
+  Form,
+  Label,
+  Input,
+  Item,
+  Content,
+  Header
+} from "native-base";
+
+// Store
+import authStore from "../../store/authStore";
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  render() {
+    const { navigation } = this.props;
+    if (authStore.user) navigation.replace("BarberList");
+    return (
+      <Content>
+        <Header transparent />
+        <List>
+          <ListItem style={{ borderBottomWidth: 0 }}>
+            <Body>
+              <Form>
+                <Body>
+                  <Label style={{ color: "white" }}>Username</Label>
+                </Body>
+                <Item
+                  rounded
+                  style={{
+                    backgroundColor: "white",
+                    marginTop: 10,
+                    marginBottom: 10
+                  }}
+                >
+                  <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    onChangeText={username => this.setState({ username })}
+                  />
+                </Item>
+                <Body>
+                  <Label style={{ color: "white" }}>Password</Label>
+                </Body>
+                <Item
+                  rounded
+                  style={{ backgroundColor: "white", marginTop: 10 }}
+                >
+                  <Input
+                    autoCorrect={false}
+                    secureTextEntry
+                    autoCapitalize="none"
+                    onChangeText={password => this.setState({ password })}
+                  />
+                </Item>
+              </Form>
+            </Body>
+          </ListItem>
+          <Button
+            full
+            success
+            onPress={() => authStore.login(this.state, navigation)}
+          >
+            <Text>Login</Text>
+          </Button>
+          <Button
+            full
+            warning
+            onPress={() => authStore.signup(this.state, navigation)}
+          >
+            <Text>Register</Text>
+          </Button>
+        </List>
+        <Body>
+          <Label style={{ color: "red", opacity: 0.6 }} />
+        </Body>
+      </Content>
+    );
+  }
+}
+
+export default Login;
